e1dd57b3a161f0b152e7f7db1b1f29a5b69d9fd8
jayval254 <valariekimuge1@gmail.com>
2023-08-11 10:39:07 -0700

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,14 +7,29 @@ import sys
 
 
 def number_of_subscribers(subreddit):
-    """ return number of subscribers"""
-    subreddit_exists = requests.get(
-        "https://reddit.com/r/{}".format(subreddit),
-        headers={'User-agent': 'test'})
-    if subreddit_exists.status_code == 200:
-        subscribers = requests.get(
-            "https://reddit.com/r/{}/about.json".format(subreddit),
-            headers={'User-agent': 'test'})
-        return (subscribers.json().get("data").get("subscribers"))
-    else:
-        return (0)
+    """ Return number of subscribers or 0 """
+    from requests import get
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    r = get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return 0
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        return 0
+
+    data = js.get("data")
+
+    if data:
+        sub_count = data.get("subscribers")
+        if sub_count:
+            return sub_count
+
+    return 0
